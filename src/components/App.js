56cce32a52a6b86395e9d446e212d1083ed8f03b
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-09-29 11:49:48 -0400

    Making API request with Axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
+import axios from 'axios';
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      const request = await axios.get(
+        'https://www.breakingbadapi.com/api/characters'
+      );
+      setCharacters(request.data);
+    };
+
+    makeRequest();
+  }, []);
+
   return (
     <div className='App container'>
       <Header />
+      {characters.map(character => (
+        <p>{character.name}</p>
+      ))}
     </div>
   );
 }

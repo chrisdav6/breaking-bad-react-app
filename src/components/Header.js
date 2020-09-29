1b4313b5204dd614a907be3997ba003aae8c5fc2
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-09-29 15:58:21 -0400

    Added link to offical page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,13 @@ import logo from '../img/logo.png';
 const Header = () => {
   return (
     <header className='center'>
-      <img src={logo} alt='Logo' />
+      <a
+        href='https://www.amc.com/shows/breaking-bad--18'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img src={logo} alt='Logo' />
+      </a>
     </header>
   );
 };

138fb9511fb1d0345a78dc9c6859661d8333d5fd
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-09-29 15:07:10 -0400

    Completed Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 const Card = ({ character }) => {
-  const { name, nickname, img } = character;
+  const { name, nickname, img, portrayed, birthday, status } = character;
 
   return (
     <div className='card'>
-      <h1>{name}</h1>
-      <p>{nickname}</p>
-      <img src={img} alt={name} />
+      <div className='card-inner'>
+        <div className='card-front'>
+          <img src={img} alt={name} />
+        </div>
+        <div className='card-back'>
+          <h1>{name}</h1>
+          <ul>
+            <li>
+              <strong>Actor Name:</strong> {portrayed}
+            </li>
+            <li>
+              <strong>Nickname:</strong> {nickname}
+            </li>
+            <li>
+              <strong>Birthday:</strong> {birthday}
+            </li>
+            <li>
+              <strong>Status:</strong> {status}
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
